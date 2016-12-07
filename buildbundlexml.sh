5ae02e31b9b3a45a64f4febc7a82a5a0fc06fa49
Arnauld Van Muysewinkel <Arnauld.VanMuysewinkel@smals.be>
2016-12-07 12:09:41 +0100

    fix(build): generate bundle.xml depending on the local dir

---
+++ buildbundlexml.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+basedir=$(dirname "$0")
+basedir=$(readlink -f "$basedir")
+hostbasedir=$(echo "$basedir" | perl -pe 's{^/([a-zA-Z])/}{\1:/}') # adjust for drive letter in case of windows, else does nothing
+
+set -x
+perl -pe "s{#BASEDIR#}{$hostbasedir}g" $basedir/bundle.xml.template > $basedir/bundle.xml
