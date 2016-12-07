8953296fc856eb520be451ecc6a30f4fd5b20ac0
Arnauld Van Muysewinkel <arnauldvm@gmail.com>
2016-12-07 14:52:42 +0100

    fix(build): make more portable (avoid using readlink)

--- buildbundlexml.sh
+++ buildbundlexml.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 basedir=$(dirname "$0")
-basedir=$(readlink -f "$basedir")
+basedir=$(cd "$basedir"; pwd) # this is more portable than readlink
 hostbasedir=$(echo "$basedir" | perl -pe 's{^/([a-zA-Z])/}{\1:/}') # adjust for drive letter in case of windows, else does nothing
 
 set -x
